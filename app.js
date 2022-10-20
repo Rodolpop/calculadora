a528782a82bffd87c37454e2f04028bbc8bd620c
rodolpop <garciasena@yahoo.com>
2022-10-20 02:50:57 +0200

    3

---
+++ app.js
@@ -0,0 +1,20 @@
+
+function init(){
+    let resultado = document.getElementById('resultado');
+    let reset = document.getElementById('reset');
+    let suma = document.getElementById('suma');
+    let resta = document.getElementById('resta');
+    let multiplicacion = document.getElementById('multiplicacion');
+    let division = document.getElementById('division');
+    let igual = document.getElementById('igual');
+    let uno = document.getElementById('uno');
+    let dos = document.getElementById('dos');
+    let tres = document.getElementById('tres');
+    let cuatro = document.getElementById('cuatro');
+    let cinco = document.getElementById('cinco');
+    let seis = document.getElementById('seis');
+    let siete = document.getElementById('siete');
+    let ocho = document.getElementById('ocho');
+    let nueve = document.getElementById('nueve');
+    let cero = document.getElementById('cero');
+}
