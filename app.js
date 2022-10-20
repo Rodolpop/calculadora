4fde236493ed5b7c000fbeb8c3c25e17b6476f0a
rodolpop <garciasena@yahoo.com>
2022-10-20 03:29:34 +0200

    Fin

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function init(){
     ocho.onclick = function(e){
         resultado.textContent = resultado.textContent + "8";
     }
-    nuevo.onclick = function(e){
+    nueve.onclick = function(e){
         resultado.textContent = resultado.textContent + "9";
     }
     cero.onclick = function(e){
@@ -61,7 +61,7 @@ function init(){
     }
 
     reset.onclick = function(e){
-        Resetear();
+        resetear();
     }
 
     suma.onclick = function(e){
@@ -69,4 +69,64 @@ function init(){
         operacion = "+";
         limpiar();
     }
+
+    resta.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+
+    multiplicacion.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+
+    division.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+
+    igual.onclick = function(e){
+        operandob = resultado.textContent;
+        resolver();
+    }
+
+}
+
+function limpiar (){
+    resultado.textContent = "";
+}
+
+function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+
+function resolver(){
+    let res = 0;
+    switch(operacion){
+
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+
+         case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
+    }
+
+    resetear();
+    resultado.textContent = res;
 }
