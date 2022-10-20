a83fd62ac933887c9afc23caf7b8fe9048fa7f0d
rodolpop <garciasena@yahoo.com>
2022-10-20 03:11:15 +0200

    4

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+  
+let operandoa;
+let operandob;
+let operacion;
+
 
 function init(){
+
+    //variables
+
+
     let resultado = document.getElementById('resultado');
     let reset = document.getElementById('reset');
     let suma = document.getElementById('suma');
@@ -17,4 +26,47 @@ function init(){
     let ocho = document.getElementById('ocho');
     let nueve = document.getElementById('nueve');
     let cero = document.getElementById('cero');
+
+    //Eventos
+
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent + "1";
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent + "2";
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent + "3";
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent + "4";
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent + "5";
+    }
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent + "6";
+    }
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent + "7";
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent + "8";
+    }
+    nuevo.onclick = function(e){
+        resultado.textContent = resultado.textContent + "9";
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent + "0";
+    }
+
+    reset.onclick = function(e){
+        Resetear();
+    }
+
+    suma.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
 }
